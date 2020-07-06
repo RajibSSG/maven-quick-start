1390dc55de24a92c6632183e9df7e043e6702a80
Banerjee, Rajib <Banerjee_Rajib@yahoo.com>
2020-07-06 21:49:54 +0530

    Modied Application.java file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -4,12 +4,14 @@ public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
+		System.out.println ("Inside Application - extra line added");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
 	
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
+		System.out.println ("Starting Application - extra line added");
 	    Application app = new Application();
          }
      }
