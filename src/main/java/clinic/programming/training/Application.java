b3dc482688c8d5ab73ed8481f6271d6b0856dbcd
Banerjee, Rajib <Banerjee_Rajib@yahoo.com>
2020-07-07 16:41:04 +0530

    Added StringUtil to Application.java and dependency added to pom.xml

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,13 @@
 package clinic.programming.training;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+	
+	public int countWords (String words)  {
+		
+		String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
+    } 		
     
     public Application() {
         System.out.println ("Inside Application");
@@ -13,5 +20,8 @@ public class Application {
     	System.out.println ("Starting Application");
 		System.out.println ("Starting Application - extra line added");
 	    Application app = new Application();
+		
+		int count = app.countWords(" I have four words");
+		System.out.println("Word Count :" +count);
          }
      }
