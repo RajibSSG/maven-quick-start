1b2026dda6472a1e44da0fbf37c54f71b5cc8de7
Banerjee, Rajib <Banerjee_Rajib@yahoo.com>
2020-07-08 16:52:37 +0530

    Modified Application.java to check PollSCM checks

--- src/test/java/clinic/programming/training/ApplicationTest.java
+++ src/test/java/clinic/programming/training/ApplicationTest.java
@@ -46,9 +46,23 @@ public class ApplicationTest {
     }
 	
 	
-	// method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
-    	System.out.println ("Main metod added to check GIT POLL SCM checks");
-	   Application app = new Application(); 
-	   )
+	  
 	   }
+	   
+	   
+	   
+	   
+	   
+	   
+	   public class Application {
+    
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	Application app = new Application();
+    }
+}
