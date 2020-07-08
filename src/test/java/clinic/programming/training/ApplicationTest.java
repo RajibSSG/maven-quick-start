91558416dd6dce6cc4a0e358837f43f9e055c946
Banerjee, Rajib <Banerjee_Rajib@yahoo.com>
2020-07-08 18:06:00 +0530

    Modified ApplicationTest.java to check PollSCM checks

--- src/test/java/clinic/programming/training/ApplicationTest.java
+++ src/test/java/clinic/programming/training/ApplicationTest.java
@@ -8,16 +8,15 @@ import static org.junit.Assert.*;
 public class ApplicationTest {
     
     public ApplicationTest() {
-        System.out.println ("Inside Application Test class");
+        System.out.println ("Inside Application Test");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application Test class");
+    	System.out.println ("Starting Application Test");
 	ApplicationTest app = new ApplicationTest();
     }
 }
-	   
 	   
 	   
 	   
