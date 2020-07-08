f0da497d0a886e9b03603fe52e247d6058848ee5
Banerjee, Rajib <Banerjee_Rajib@yahoo.com>
2020-07-08 17:33:58 +0530

    Modified ApplicationTest.java to check PollSCM checks

--- src/test/java/clinic/programming/training/ApplicationTest.java
+++ src/test/java/clinic/programming/training/ApplicationTest.java
@@ -6,51 +6,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ApplicationTest {
-	
-	System.out.println ("I am inside ApplicationTest class");
-	
-	    private Application app;
     
-    @Before
-    public void setup() {
-        app = new Application();
-    }
-    
-    
-    @Test
-    public void testWordCountString() {
-        int count = app.countWords("this is a test");
-        assertTrue(count == 4);
+    public ApplicationTest() {
+        System.out.println ("Inside Application Test class");
     }
 
-    @Test
-    public void testWordCountEmpty() {
-        int count = app.countWords("");
-        assertTrue(count == 0);
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application Test class");
+	Application app = new Application();
     }
-
-    @Test
-    public void testWordCountNull() {
-        int count = app.countWords(null);
-        assertTrue(count == 0);
-    }
-
-    @Test
-    @Ignore
-    public void testWordCountMore() {
-        int count = app.countWords("this will be skipped");
-        assertTrue(count == 3);
-    }
-
-    @Test
-    public void testWordCountSingle() {
-        int count = app.countWords("test");
-        assertTrue(count == 1);
-    }
-	
-	
-	  
-	   }
+}
 	   
 	   
 	   
